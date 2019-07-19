9a4562ca10256d7d839ecde081c6fce0e2725af8
Jeison Cardoso <2099388+0um@users.noreply.github.com>
2019-07-19 11:04:27 -0300

    Build support for linux

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # fusion_bcf
 Command line tool to merge two bcf files
-
-[![Codacy Badge](https://api.codacy.com/project/badge/Grade/186af04a24f244ce841d6802f2e3721c)](https://www.codacy.com/app/0um/fusion_bcf?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=0um/fusion_bcf&amp;utm_campaign=Badge_Grade)
-
-**Only Windows Platform!**
+[![Build Status](https://travis-ci.org/0um/fusion_bcf.svg?branch=master)](https://travis-ci.org/0um/fusion_bcf)[![Build status](https://ci.appveyor.com/api/projects/status/4fhtr1v83thrcjun?svg=true)](https://ci.appveyor.com/project/0um/fusion-bcf)[![Codacy Badge](https://api.codacy.com/project/badge/Grade/186af04a24f244ce841d6802f2e3721c)](https://www.codacy.com/app/0um/fusion_bcf?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=0um/fusion_bcf&amp;utm_campaign=Badge_Grade)
 
 Build
-````
+```
 cmake -G "Visual Studio 15 2017"
 ```
 
@@ -15,7 +12,7 @@ Run
 fusion_bcf.exe inputA inputB <optional OutputC>
 ```
 
-Note rapidxml is over.
+Note rapidxml under licenses
 1. Boost Software License - Version 1.0 - August 17th, 2003
 2. The MIT License
 
--- src/Timestamp.cpp
+++ src/Timestamp.cpp
@@ -4,15 +4,13 @@
 #include <iomanip>
 #include <iterator>
 #include <sstream>
+#include <string>
 
-#pragma warning(push, 0) 
 #include "cctz/civil_time.h"
 #include "cctz/time_zone.h"
 #include "cctz/zone_info_source.h"
-#pragma warning(pop) 
 
-using namespace std;
-
+#ifdef _WIN32
 namespace {
   #include <Windows.h>
 
@@ -22,12 +20,18 @@ namespace {
     return (long)TimeZoneInfo.Bias;
   }
 }
+#endif
+#ifdef linux
+  long SystemTimeZoneOffSetMinutes() {
+    return (long)0;
+  }
+#endif
 
 Timestamp::Timestamp() {
   tp = std::chrono::system_clock::now();
 }
 
-Timestamp::Timestamp(const string& timestamp, const string& format) {
+Timestamp::Timestamp(const std::string& timestamp, const std::string& format) {
   const auto utc = cctz::utc_time_zone();
   if (!cctz::parse(format.c_str(), timestamp.c_str(), utc, &tp)) {
     tp = std::chrono::system_clock::now();
@@ -38,18 +42,18 @@ bool Timestamp::operator<(const Timestamp& other) const {
   return tp < other.tp;
 }
 
-const string Timestamp::InUTCTime(const string& format) const {
+const std::string Timestamp::InUTCTime(const std::string& format) const {
   const auto utc = cctz::utc_time_zone();
   return cctz::format(format.c_str(), tp, utc).c_str();
 }
 
-const string Timestamp::InLocalTime(const string& format) const {
+const std::string Timestamp::InLocalTime(const std::string& format) const {
   cctz::sys_seconds offsetSeconds(SystemTimeZoneOffSetMinutes() * 60);
   const auto local = cctz::fixed_time_zone(-offsetSeconds);
   return cctz::format(format.c_str(), tp, local).c_str();
 }
 
-bool Timestamp::IsValid(const string& timestamp, const string& format) {
+bool Timestamp::IsValid(const std::string& timestamp, const std::string& format) {
   std::chrono::system_clock::time_point tmpTp;
   const auto utc = cctz::utc_time_zone();
   return cctz::parse(format.c_str(), timestamp.c_str(), utc, &tmpTp);
